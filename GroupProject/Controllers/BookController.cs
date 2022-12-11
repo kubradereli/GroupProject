706c256b6f646722c2bc2178576b3ddd4259b7b7
selinnurgol <selinnur.gol@ogr.sakarya.edu.tr>
2022-12-11 15:23:10 +0300

    kitaplar sayfası oluşturuldu ve kitaplar listelendi

---
+++ GroupProject/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Controllers
+{
+    public class BookController : Controller
+    {
+        BookManager bookManager = new BookManager(new EfBookRepository());
+
+        public IActionResult Index()
+        {
+            var values = bookManager.TGetList();
+            return View(values);
+        }
+    }
+}
