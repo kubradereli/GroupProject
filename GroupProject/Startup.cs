8ff8b39650ba77957bb71792d0c1ef00491f03ca
kubradereli <0kubradereli@gmail.com>
2022-12-11 13:52:13 +0300

    Anasayfa oluşturuldu.

--- GroupProject/Startup.cs
+++ GroupProject/Startup.cs
@@ -50,7 +50,7 @@ namespace GroupProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=About}/{action=Index}/{id?}");
             });
         }
     }
