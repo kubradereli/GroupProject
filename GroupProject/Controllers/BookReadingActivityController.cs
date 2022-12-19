ad62035415321d4fa8a3b73af6fe4ec60a8eb9eb
kubradereli <0kubradereli@gmail.com>
2022-12-20 00:05:04 +0300

    About sayfasında düzenlemeler yapıldı.

---
+++ GroupProject/Controllers/BookReadingActivityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Controllers
+{
+    public class BookReadingActivityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
